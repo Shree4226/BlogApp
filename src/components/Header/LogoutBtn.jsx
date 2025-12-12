b892849e911f50214728ddc691b1a46ceac66e28
Shree4226 <shreesim4226@gmail.com>
2025-12-12 12:01:52 +0530

    Refactor LogoutBtn component for new auth service
    Updated import path for authService and improved logout handler comments.

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { useDispatch } from "react-redux"
-import authService from '../../../appwrite/auth'
-import {logout} from '../../store/authSlice'
+import { useDispatch } from "react-redux";
+// UPDATED: Import from the new services folder
+import authService from '../../services/auth'; 
+import { logout } from '../../store/authSlice';
 
-function LogoutBtn(){
-    const dispatch = useDispatch()
-    const logoutHandeler = () => {
+function LogoutBtn() {
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        // The new MERN authService.logout() calls the backend to clear the cookie
         authService.logout().then(() => {
-            dispatch(logout())
-        })
-    }
+            // Once backend confirms logout, we clear the Redux state
+            dispatch(logout());
+        });
+    };
+
     return (
-        <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
-        onClick={logoutHandeler}>Logout
+        <button
+            className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+            onClick={logoutHandler}
+        >
+            Logout
         </button>
-    )
+    );
 }
 
 export default LogoutBtn;
